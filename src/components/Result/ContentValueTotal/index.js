ab88e3d3bb3a9550db68db2d347baeee9bbcc902
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-02-28 10:27:32 -0300

    Remoção de linhas desnecessárias

--- src/components/Result/ContentValueTotal/index.js
+++ src/components/Result/ContentValueTotal/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles.css';
 
 const ContentValueTotal  = ({ format }) => (
-
   <header className="ContentValueTotal">
     <h4 className="result-content-title">
       Valor Total
@@ -16,7 +15,6 @@ const ContentValueTotal  = ({ format }) => (
       </li>
     </ul>
   </header>
-
 );
 
 export default ContentValueTotal;
